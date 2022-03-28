dea4ebaf0e3157ab4a9dbff7bd8ca7b9afd9e184
myg0t <mjtaylorroberts@gmail.com>
2022-03-28 00:13:16 -0400

    add comments

--- blockchain/contracts/TodoList.sol
+++ blockchain/contracts/TodoList.sol
@@ -19,27 +19,28 @@ contract TodoList {
     mapping (uint => address) taskToOwner;
     mapping (address => uint[]) ownerTaskIds;
 
-    // REMOVE
-    // mapping (address => uint) ownerTaskCount;
-    // mapping (address => Task[]) ownerTasks;
-
     constructor() {}
 
     function getOwnerTasks() public view returns (Task[] memory) {
+        // Create a copy of the requester's task ID's
         uint[] memory taskIds = ownerTaskIds[msg.sender];
+
+        // Create a new Task[] with a predefined length
         Task[] memory _tasks = new Task[](taskIds.length);
 
+        // Loop through each of the requester's task ID's
         for (uint i = 0; i < taskIds.length; i++) {
+            // Assign Task objects to the _tasks array
             _tasks[i] = tasks[taskIds[i]];
-            // _tasks.push(tasks[taskIds[i]]);
         }
+
+        // Return owner's Task objects
         return tasks;
-        // return ownerTasks[msg.sender];
     }
 
     function getOwnerTaskCount() public view returns (uint) {
+        // Return the length of the requester's tasks
         return ownerTaskIds[msg.sender].length;
-        // return ownerTaskCount[msg.sender];
     }
 
     function createTask(string memory _content) public {
@@ -55,27 +56,23 @@ contract TodoList {
         // Add task id to ownerTaskIds
         ownerTaskIds[msg.sender].push(id);
 
-        // Task storage task = tasks[id];
-        // taskToOwner[id] = msg.sender;
-        // ownerTaskIds.push(id);
-
-        // REMOVE
-        // ownerTasks[msg.sender].push(task);
-
-        // taskToOwner[id] = msg.sender;
-        // ownerTaskCount[msg.sender]++;
-        // ownerTasks[msg.sender].push(_task);
-
+        // Emit Task Created Event
         emit TaskCreated(taskCount, _content, false);
+
+        // Increment Task Count by 1
+        // This will also be the next task's ID.
         taskCount++;
     }
 
     function toggleCompleted(uint256 _id) public {
+        // Reference task from storage
         Task storage _task = tasks[_id];
+
+        // Switch completed value
+        // Because we specified storage above, the change get's applied to `tasks[_id]`
         _task.completed = !_task.completed;
-        // Task memory _task = tasks[_id];
-        // _task.completed = !_task.completed;
-        // tasks[_id] = _task;
+
+        // Emit TaskCompleted Event
         emit TaskCompleted(_id, _task.completed);
     }
 }
